70f857afa08a4722508d7ad4e7b82913921da2ca
Rubens Mariuzzo <rubens@mariuzzo.com>
2017-06-19 00:08:41 -0400

    Handle PHP file header/footer.

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const parser = require('php-array-parser')
 
 module.exports = (source) => {
-  const value = parser.parse(source)
+  // Remove left part of return expression and any ending `?>`.
+  const ret = source.indexOf('return') + 'return'.length
+  source = source.substr(ret)
+  source = source.replace(/\?>\s*$/, '_')
+
+  const value = JSON.stringify(parser.parse(source))
   return this.version && this.version >= 2 ? `export default ${value};` : `module.exports = ${value};`
 }
